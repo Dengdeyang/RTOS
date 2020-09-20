80c86191ea77ab1ba007d02141355f1badbf598a
Dengdeyang <2466848612@qq.com>
2020-09-20 20:25:42 +0800

    Update RTOS.c

--- RTOS.c
+++ RTOS.c
@@ -224,11 +224,11 @@ __ASM void PendSV_Handler(void)//有硬件fault
 	LDR R4,=__cpp(&next_task_id);//把C语言中全局变量next_task_id的地址存入R4，汇编调用C变量需用cpp函数
 	LDR R4,[R4]//将next_task_id数值存入R4中
 	STR R4,[R1]//将current_task_id = next_task_id
-	LDR R0,[R3,R4,LSL #2]//将地址=R3+R2<<2处数据加载到R0，(PSP_array+next_task_id+2处数据加载到PSP处)
+	LDR R0,[R3,R4,LSL #2]//将地址=R3+R2<<2处数据加载到R0，(PSP_array+next_task_id*2处数据加载到PSP处)
 	LDMIA R0!,{R4 - R11}//PSP所指地址处读出数据加载到R4~R11中，每存一次R0更新地址，记录PSP当前值
 	MSR PSP,R0  //R0所指地址加载到PSP
 	BX LR      //PC返回到LR所指处，该函数调用前地址
 	ALIGN 4    //下一条指令或数据对齐到4字节地址处，空位补0
 }
-
+ 
 /*********************************************END OF FILE**********************/
